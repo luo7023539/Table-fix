5e5d61237b8639681be26aacdf52415a7581c929
luoliang01 <luoliang01@58ganji.com>
2018-01-16 19:28:01 +0800

    Progress

--- src/progress.js
+++ src/progress.js
@@ -5,12 +5,13 @@
  */
 const __wrapStyle = {
   padding: '2px 4px',
-  'background-color': '#fff'
+  'background-color': '#fff',
+  position: 'relative',
+  height: '20px'
 }
 
 const __baseStyle = {
   width: '100%',
-  'background-color': '#ccc'
 }
 
 const __labelStyle = {
@@ -20,10 +21,11 @@ const __labelStyle = {
   transform: 'translate(-50%, -50%)'
 }
 
-export default (dom, style, {rate, value}) => {
+export default (dom, style, { rate, value }, isDB) => {
   const wrap = document.createElement('div')
   const progress = document.createElement('div')
-  const label = document.createElement('div')
+
+  rate = parseInt(rate)
 
   const wStyle = wrap.style;
   Object.assign(wStyle, __wrapStyle)
@@ -32,14 +34,32 @@ export default (dom, style, {rate, value}) => {
    */
   const pStyle = progress.style
   Object.assign(pStyle, __baseStyle, style)
-  progress.style.width = parseInt(rate) > 100 ? '100%' : rate
+  /**
+   * 百分比超出 - 则设置为100%
+   */
+  progress.style.width = rate > 100 || rate < -100
+    ? '100%'
+    : Math.abs(rate) + '%'
   wrap.appendChild(progress)
   /**
-   * 
+   * 普通进度条 采用居中label
+   * 双向进度条 位于progress.innerText
    */
-  const lStyle = label.style
-  Object.assign(lStyle, __labelStyle)
-  label.innerText = value
-  wrap.appendChild(label)
+
+  if (!isDB) {
+    const label = document.createElement('div')
+    const lStyle = label.style
+    Object.assign(lStyle, __labelStyle)
+    label.innerText = value
+    wrap.appendChild(label)
+  
+  } else {
+    if (0 >= rate && rate > -100) {
+      pStyle.float = 'right'
+    } else if (rate > 0){
+      pStyle.float = 'left'
+    }
+    progress.innerText = value
+  }
   return dom.appendChild(wrap)
 };
